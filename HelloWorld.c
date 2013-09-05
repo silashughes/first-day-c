b73552dd1d2626b790174f50caaceeda162d332d
Silas Hughes <silas.hughes@students.olin.edu>
2013-09-05 15:13:00 -0400

    Added notes

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,24 @@
 #include <stdint.h>
 
 int32_t main(void) {
-    printf("Sorry, I can't do that Dave.\n");
+    printf("Hello Drew!\n");
     return 1;
 }
+
+/* Notes
+
+To compile, gcc Helloworld.c -o Helloworld
+To run, ./Helloword.exe
+To show code, less Helloworld.c
+
+
+&& means "and"
+|| means "or"
+
+& bitwise and
+| bitwise or
+
+0 means false, non-0 means true
+
+Review tutorials on C
+*/
